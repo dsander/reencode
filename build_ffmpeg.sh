3964474b8c9324e84db345675c04d45797ab8f76
Dominik Sander <git@dsander.de>
2018-11-03 21:16:02 +0100

    Update ffmpeg build script to work on ubuntu 18 and latest nvidia drivers
    Doing a static build now segfault, so avoid this we have to modify
    LD_LIBRARY_PATH so that ffmpeg find the libraries it was linked against.

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
-# Install cuda beforehand:
 
-# wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-# sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-# apt-get -y update && apt-get -y install cuda
+# Install CUDA and NVIDIA driver:
 
+# wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+# sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+# apt-get update
+# apt-get install cuda
+# echo "export PATH=/usr/local/cuda-10.0/bin:~/bin:\$PATH" >> ~/.bashrc
+# echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:~/ffmpeg_build/lib/"  >> ~/.bashrc
 
-#This script will compile and install a static ffmpeg build with support for nvenc un ubuntu.
-#See the prefix path and compile options if edits are needed to suit your needs.
+# This script will compile and install a ffmpeg build with support for nvenc on ubuntu.
+# See the prefix path and compile options if edits are needed to suit your needs.
 
 #install required things from apt
 installLibs(){
@@ -16,16 +19,18 @@ echo "Installing prerequisites"
 sudo apt-get update
 sudo apt-get -y --force-yes install autoconf automake build-essential libass-dev libfreetype6-dev libgpac-dev \
   libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
-  libxcb-xfixes0-dev pkg-config texi2html zlib1g-dev cmake mercurial
+  libxcb-xfixes0-dev pkg-config texi2html zlib1g-dev cmake mercurial nasm yasm
 }
 
 #Compile libx265
 compileLibX265(){
 echo "Compiling libx265"
 cd ~/ffmpeg_sources
+rm -rf x265
 hg clone https://bitbucket.org/multicoreware/x265
 cd ~/ffmpeg_sources/x265/build/linux
-PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off -DHIGH_BIT_DEPTH:bool=ON ../../source
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DHIGH_BIT_DEPTH:bool=ON -DENABLE_CLI:BOOL=OFF ../../source
+sed -i 's/-lgcc_s/-lgcc_eh/g' x265.pc
 make -j$(nproc)
 make -j$(nproc) install
 make -j$(nproc) clean
@@ -35,10 +40,11 @@ make -j$(nproc) clean
 compileLibX264(){
 echo "Compiling libx264"
 cd ~/ffmpeg_sources
+rm -rf last_x264.tar.bz2 x264-snapshot*
 wget http://download.videolan.org/pub/x264/snapshots/last_x264.tar.bz2
 tar xjvf last_x264.tar.bz2
 cd x264-snapshot*
-PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --bit-depth=10
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-shared --enable-pic
 PATH="$HOME/bin:$PATH" make -j$(nproc)
 make -j$(nproc) install
 make -j$(nproc) distclean
@@ -49,11 +55,12 @@ compileLibfdkcc(){
 echo "Compiling libfdk-cc"
 sudo apt-get install unzip
 cd ~/ffmpeg_sources
+rm -rf fdk-aac.zip mstorsjo-fdk-aac*
 wget -O fdk-aac.zip https://github.com/mstorsjo/fdk-aac/zipball/master
 unzip fdk-aac.zip
 cd mstorsjo-fdk-aac*
 autoreconf -fiv
-./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+./configure --prefix="$HOME/ffmpeg_build"
 make -j$(nproc)
 make -j$(nproc) install
 make -j$(nproc) distclean
@@ -64,10 +71,11 @@ compileLibMP3Lame(){
 echo "Compiling libmp3lame"
 sudo apt-get install nasm
 cd ~/ffmpeg_sources
-wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
-tar xzvf lame-3.99.5.tar.gz
-cd lame-3.99.5
-./configure --prefix="$HOME/ffmpeg_build" --enable-nasm --disable-shared
+rm -rf lame*
+wget http://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz
+tar xzvf lame-3.100.tar.gz
+cd lame-3.100
+./configure --prefix="$HOME/ffmpeg_build" --enable-nasm
 make -j$(nproc)
 make -j$(nproc) install
 make -j$(nproc) distclean
@@ -77,10 +85,11 @@ make -j$(nproc) distclean
 compileLibOpus(){
 echo "Compiling libopus"
 cd ~/ffmpeg_sources
-wget http://downloads.xiph.org/releases/opus/opus-1.1.3.tar.gz
-tar xzvf opus-1.1.3.tar.gz
-cd opus-1.1.3
-./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+rm -rf opus*
+wget https://ftp.osuosl.org/pub/xiph/releases/opus/opus-1.3.tar.gz
+tar xzvf opus-1.3.tar.gz
+cd opus-1.3
+./configure --prefix="$HOME/ffmpeg_build"
 make -j$(nproc)
 make -j$(nproc) install
 make -j$(nproc) distclean
@@ -90,32 +99,41 @@ make -j$(nproc) distclean
 compileLibPvx(){
 echo "Compiling libvpx"
 cd ~/ffmpeg_sources
-wget storage.googleapis.com/downloads.webmproject.org/releases/webm/libvpx-1.6.1.tar.bz2
-tar xjvf libvpx-1.6.1.tar.bz2
-cd libvpx-1.6.1/
-PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples
+rm -rf libvpx*
+wget https://github.com/webmproject/libvpx/archive/v1.7.0.tar.gz
+tar xvf v1.7.0.tar.gz
+cd libvpx-1.7.0
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples --enable-pic
 PATH="$HOME/bin:$PATH" make -j$(nproc)
 make -j$(nproc) install
 make -j$(nproc) clean
+}
+
+compileNvCodecHeaders() {
+cd ~/ffmpeg_sources
+rm -rf nv-codec-headers
+git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+cd nv-codec-headers
+make install PREFIX="$HOME/ffmpeg_build"
 }
 
 #Compile ffmpeg
 compileFfmpeg(){
 echo "Compiling ffmpeg"
 cd ~/ffmpeg_sources
-wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+rm -rf ffmpeg*
+wget https://www.ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
 tar xjvf ffmpeg-snapshot.tar.bz2
 cd ffmpeg
+
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
   --prefix="$HOME/ffmpeg_build" \
-  --pkg-config-flags="--static" \
-  --extra-cflags="-I$HOME/ffmpeg_build/include" \
-  --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+  --extra-cflags="-I$HOME/ffmpeg_build/include -I/usr/local/cuda/include/" \
+  --extra-ldflags="-L$HOME/ffmpeg_build/lib -L/usr/local/cuda/lib64/" \
+  --extra-libs="-lpthread -lm" \
   --bindir="$HOME/bin" \
   --enable-gpl \
-  --enable-libass \
   --enable-libfdk-aac \
-  --enable-vaapi \
   --enable-libfreetype \
   --enable-libmp3lame \
   --enable-libopus \
@@ -125,11 +143,12 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --enable-libx264 \
   --enable-libx265 \
   --enable-nonfree \
+  --nvccflags="-gencode arch=compute_61,code=sm_61 -O2" \
   --enable-nvenc
+  # --enable-libass \
+  # --enable-vaapi \
 PATH="$HOME/bin:$PATH" make -j$(nproc)
 make -j$(nproc) install
-make -j$(nproc) distclean
-hash -r
 }
 
 #The process
@@ -142,5 +161,6 @@ compileLibfdkcc
 compileLibMP3Lame
 compileLibOpus
 compileLibPvx
+compileNvCodecHeaders
 compileFfmpeg
 echo "Complete!"
